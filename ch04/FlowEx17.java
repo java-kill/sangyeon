8bbc25c8d7f29039af31f751233253a0a6f39041
ysang989 <isp3541@gmail.com>
2023-10-13 18:00:08 +0900

    week01-ch03,04

---
+++ ch04/FlowEx17.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class FlowEx17 {
+    public static void main(String[] args) {
+        int num = 0;
+
+        System.out.print("*을 출력할 라인의 수를 입력하세요.>");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        num = Integer.parseInt(tmp);
+
+        for(int i=0;i<num;i++) {
+            for(int j=0;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    } // main의 끝 , i=1부터 해도 똑같은 결과였다.
+}
